23cb2c465a5a1fd30c2d782031085e2c14281d79
pkkirilov <peter_kirilov@yahoo.com>
2017-07-06 16:52:54 +0100

    part

---
+++ MM702/Assignment 1 code parts (2).py
@@ -0,0 +1,263 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 15 08:20:10 2017
+
+@author: HP
+"""
+
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @pkkirilov
+ Sign out
+ Unwatch 1
+  Star 0
+  Fork 0 pkkirilov/Python
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
+Branch: master Find file Copy pathPython/Supervised Learning with scikit-learn DataCamp
+0c671fc  21 hours ago
+@pkkirilov pkkirilov Update Supervised Learning with scikit-learn DataCamp
+1 contributor
+RawBlameHistory     
+237 lines (171 sloc)  6.53 KB
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors = 6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
+
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits["DESCR"])
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
+
+# Import necessary modules
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# Create feature and target arrays
+X = digits.data
+y = digits.target
+
+# Split into training and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=42, stratify=y)
+
+# Create a k-NN classifier with 7 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors = 7)
+
+# Fit the classifier to the training data
+knn.fit(X_train, y_train)
+
+# Print the accuracy
+print(knn.score(X_test, y_test))
+
+# Setup arrays to store train and test accuracies
+neighbors = np.arange(1, 9)
+train_accuracy = np.empty(len(neighbors))
+test_accuracy = np.empty(len(neighbors))
+
+# Loop over different values of k
+for i, k in enumerate(neighbors):
+    # Setup a k-NN Classifier with k neighbors: knn
+    knn = KNeighborsClassifier(n_neighbors=k)
+
+    # Fit the classifier to the training data
+    knn.fit(X_train, y_train)
+    
+    #Compute accuracy on the training set
+    train_accuracy[i] = knn.score(X_train, y_train)
+
+    #Compute accuracy on the testing set
+    test_accuracy[i] = knn.score(X_test, y_test)
+
+# Generate plot
+plt.title('k-NN: Varying Number of Neighbors')
+plt.plot(neighbors, test_accuracy, label = 'Testing Accuracy')
+plt.plot(neighbors, train_accuracy, label = 'Training Accuracy')
+plt.legend()
+plt.xlabel('Number of Neighbors')
+plt.ylabel('Accuracy')
+plt.show()
+# It looks like the test accuracy is highest when using 3 and 5 neighbors. Using 8 neighbors or more seems to result in a simple model that underfits the data.
+
+### Regression
+# Import numpy and pandas
+import numpy as np
+import pandas as pd
+
+# Read the CSV file into a DataFrame: df
+df = pd.read_csv("gapminder.csv")
+
+# Create arrays for features and target variable
+y = df["life"].values
+X = df["fertility"].values
+
+# Print the dimensions of X and y before reshaping
+print("Dimensions of y before reshaping: {}".format(y.shape))
+print("Dimensions of X before reshaping: {}".format(X.shape))
+
+# Reshape X and y
+y = y.reshape(-1,1)
+X = X.reshape(-1,1)
+
+# Print the dimensions of X and y after reshaping
+print("Dimensions of y after reshaping: {}".format(y.shape))
+print("Dimensions of X after reshaping: {}".format(X.shape))
+
+# Import numpy and pandas
+import numpy as np
+import pandas as pd
+
+# Read the CSV file into a DataFrame: df
+df = pd.read_csv("gapminder.csv")
+
+# Create arrays for features and target variable
+y = df["life"].values
+X = df["fertility"].values
+
+# Print the dimensions of X and y before reshaping
+print("Dimensions of y before reshaping: {}".format(y.shape))
+print("Dimensions of X before reshaping: {}".format(X.shape))
+
+# Reshape X and y
+y = y.reshape(-1,1)
+X = X.reshape(-1,1)
+
+# Print the dimensions of X and y after reshaping
+print("Dimensions of y after reshaping: {}".format(y.shape))
+print("Dimensions of X after reshaping: {}".format(X.shape))
+
+
+# Import necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+
+# Create training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=42)
+
+# Create the regressor: reg_all
+reg_all = LinearRegression()
+
+# Fit the regressor to the training data
+reg_all.fit(X_train, y_train)
+
+
+# Predict on the test data: y_pred
+y_pred = reg_all.predict(X_test)
+
+# Compute and print R^2 and RMSE
+print("R^2: {}".format(reg_all.score(X_test, y_test)))
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print("Root Mean Squared Error: {}".format(rmse))
+
+# Import the necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Compute 5-fold cross-validation scores: cv_scores
+cv_scores = cross_val_score(reg, X, y, cv = 5)
+
+# Print the 5-fold cross-validation scores
+print(cv_scores)
+
+print("Average 5-Fold CV Score: {}".format(np.mean(cv_scores)))
+
+# Import necessary modules
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+
+# Create a linear regression object: reg
+reg = LinearRegression()
+
+# Perform 3-fold CV
+cvscores_3 = cross_val_score(reg, X, y, cv = 3)
+print(np.mean(cvscores_3))
+
+# Perform 10-fold CV
+cvscores_10 = cross_val_score(reg, X, y, cv = 10)
+print(np.mean(cvscores_10))
+
+# Import lasso
+from sklearn.linear_model import Lasso
+
+# Instantiate a lasso regressor: lasso
+lasso = Lasso(alpha = 0.4, normalize = True)
+
+# Fit the regressor to the data
+lasso.fit(X, y)
+
+# Compute and print the coefficients
+lasso_coef = lasso.coef_
+print(lasso_coef)
+
+# Plot the coefficients
+plt.plot(range(len(df_columns)), lasso_coef)
+plt.xticks(range(len(df_columns)), df_columns.values, rotation=60)
+plt.margins(0.02)
+plt.show()
+
+
+# Import necessary modules
+from sklearn.linear_model import Ridge
+from sklearn.model_selection import cross_val_score
+
+# Setup the array of alphas and lists to store scores
+alpha_space = np.logspace(-4, 0, 50)
+ridge_scores = []
+ridge_scores_std = []
+
+# Create a ridge regressor: ridge
+ridge = Ridge(normalize = True)
+
+# Compute scores over range of alphas
+for alpha in alpha_space:
+
+    # Specify the alpha value to use: ridge.alpha
+    ridge.alpha = alpha
+    
+    # Perform 10-fold CV: ridge_cv_scores
+    ridge_cv_scores = cross_val_score(ridge, X, y, cv = 10)
+    
+    # Append the mean of ridge_cv_scores to ridge_scores
+    ridge_scores.append(np.mean(ridge_cv_scores))
+    
+    # Append the std of ridge_cv_scores to ridge_scores_std
+    ridge_scores_std.append(np.std(ridge_cv_scores))
+
+# Display the plot
+display_plot(ridge_scores, ridge_scores_std)
+Contact GitHub API Training Shop Blog About
+© 2017 GitHub, Inc. Terms Privacy Security Status Help
