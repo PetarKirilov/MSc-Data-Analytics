ffc00f19f0251d8da23b3a65587f8e1c5ebba89a
pkkirilov <peter_kirilov@yahoo.com>
2017-11-08 09:21:34 +0000

    .

---
+++ MM707/w03 MM707 Poisson and exp dist.R
@@ -0,0 +1,34 @@
+#ex1
+  #ex1 from problem sheet
+#a)
+dpois(2,0.5*8) 
+#b)
+ppois(2,5*0.5)
+  #ex3
+1-ppois(16,12)
+  #ex4
+#a)
+dexp(1,10/2)
+#b)
+dexp(2,10/2)
+#c)
+dpois(6,10/2)
+#ex2
+lambda <- 10
+tmax <- 1
+n <- 100
+dt <- T/n
+t <- (1:n)*dt
+p <- lambda*dt
+q <- 1-p
+x <- sample(c(0,1),n,prob=c(q,p),replace=TRUE)
+sum(x)
+t[x==1]
+event.t <- t[x==1]
+plot(event.t, 0*event.t)
+#ex3
+plusone <- function(x) {
+  y <- x+1
+  return(y)
+}
+#ex4
