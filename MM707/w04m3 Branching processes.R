b506459b54f44608ff14a438d8ac0a812c6299ad
pkkirilov <peter_kirilov@yahoo.com>
2017-11-19 18:35:47 +0000

    branching

---
+++ MM707/w04m3 Branching processes.R
@@ -0,0 +1,9 @@
+## Simulate the Galton-Walson process with offspring variable X ~ Poisson(mu)
+
+gwn <- function(nGen,mu=1,x0=1){
+ x <- x0;                     # set the initial population size
+ for( k in 1:nGen ){          # loop over nGen generations
+  x = rpois(1,lambda=x*mu)    # sample the number of offsprings
+ }
+ return(x)                    # return the final population size
+}
